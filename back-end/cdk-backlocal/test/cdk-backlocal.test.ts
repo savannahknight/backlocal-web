99b676888e824d1401c5846fa365ec41883036f9
Mason Spong <mason.spong@gmail.com>
2022-03-23 19:45:22 -0600

    initialize cdk

---
+++ back-end/cdk-backlocal/test/cdk-backlocal.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkBacklocal from '../lib/cdk-backlocal-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-backlocal-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkBacklocal.CdkBacklocalStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
