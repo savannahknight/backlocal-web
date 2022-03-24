09c887e21f80adad7b8bf6a357dedf0b14727444
Mason Spong <mason.spong@gmail.com>
2022-03-23 20:03:21 -0600

    add aurora serverless DB

--- back-end/cdk-backlocal/lib/cdk-backlocal-stack.ts
+++ back-end/cdk-backlocal/lib/cdk-backlocal-stack.ts
@@ -1,16 +1,21 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as rds from "aws-cdk-lib/aws-rds";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkBacklocalStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkBacklocalQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const backLocalCluster = new rds.ServerlessCluster(this, "BackLocalCluster", {
+      engine: rds.DatabaseClusterEngine.AURORA_MYSQL,
+      scaling: {
+        autoPause: Duration.hours(20),
+        minCapacity: rds.AuroraCapacityUnit.ACU_8,
+        maxCapacity: rds.AuroraCapacityUnit.ACU_32,
+      },
+      defaultDatabaseName: "backLocal",
+      enableDataApi: true
+    });
   }
 }
